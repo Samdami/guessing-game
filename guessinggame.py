1f5e8289ae5525e9cc48d060c21259f40054cd54
Samuel Damilola Akinmarin <111143232+Samdami@users.noreply.github.com>
2023-05-12 14:39:46 -0700

    added a random to guessinggame.py
    i added random.randiant as the secret_number to get a random number

--- guessinggame.py
+++ guessinggame.py
@@ -1,11 +1,18 @@
-secret_number = 6
+import random
+
+number = random.randint(1, 10)
+
+secret_number = number
 guess_count = 0
 guess_limit = 3
 while guess_count < guess_limit:
-    guess_count +=1
-    guess =int(input('guess: '))
+    guess_count += 1
+    guess = int(input("guess: "))
+
     if guess == secret_number:
-        print('YOU WON!')
+        print("YOU WON!")
         break
+    elif guess != number:
+        print("try again")
 else:
-    print('SORRY YOU FAILED!')
+    print("SORRY YOU FAILED!")
